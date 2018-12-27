094bf1a4c41924decc5635d7fc0c4a3dd12ec673
Mariia Kostornychenko <m.kostornychenko@gmail.com>
2018-12-27 11:00:28 +0200

    init commit

---
+++ src/components/Home.vue
@@ -0,0 +1,91 @@
+<template>
+    <div>
+        <v-container fluid>
+            <v-layout row>
+                <v-flex xs12>
+                    <v-carousel>
+                        <v-carousel-item v-for="ad of ads" :key="ad.id" :src="ad.imgSrc">
+                            <div class="carousel-link">
+                                <v-btn class="error" :to="'/ad/' + ad.id">{{ ad.title }}</v-btn>
+                            </div>
+                        </v-carousel-item>
+                    </v-carousel>
+                </v-flex>
+            </v-layout>
+        </v-container>
+        <v-container grid-list-lg>
+            <h1>Home</h1>
+            <v-layout row wrap>
+                <v-flex xs12 sm6 md4 v-for="ad of ads" :key="ad.id">
+                    <v-card>
+                        <v-img :src="ad.imgSrc" height="200px"></v-img>
+
+                        <v-card-title primary-title>
+                            <div>
+                                <h3 class="headline mb-0">{{ ad.title }}</h3>
+                                <div>{{ ad.desctiption }}</div>
+                            </div>
+                        </v-card-title>
+
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn flat :to="'/ad/' + ad.id">Open</v-btn>
+                            <v-btn raised class="primary">Buy</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      ads: [
+        {
+          title: 'First ad',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, voluptate.',
+          promo: false,
+          imgSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          id: '101'
+        },
+        {
+          title: 'Second ad',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, voluptate.',
+          promo: false,
+          imgSrc: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          id: '102'
+        },
+        {
+          title: 'Third ad',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, voluptate.',
+          promo: false,
+          imgSrc: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          id: '103'
+        },
+        {
+          title: 'Fourth ad',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, voluptate.',
+          promo: false,
+          imgSrc: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          id: '104'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .carousel-link {
+        position: absolute;
+        bottom: 50px;
+        left: 50%;
+        background: rgba(0, 0, 0, .5);
+        transform: translateX(-50%);
+        padding: 5px 10px;
+        border-radius: 5px 5px 0 0;
+    }
+</style>
